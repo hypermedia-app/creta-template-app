5d2fdadaa4c30df8fe5961bcc28462c2e2aa242d
tpluscode <tpluscode@users.noreply.github.com>
2022-02-24 21:04:10 +0100

    refacotr: change dir name

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,4 +1,3 @@
 """
 {{ cookiecutter.update({"apis": cookiecutter.apis.split(',')}) }}
-{{ cookiecutter.update({"project_slug": cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}
 """
