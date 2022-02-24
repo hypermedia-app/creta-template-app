1a503db9a0d374fd097c1a8eec23b1c1136b6238
tpluscode <tpluscode@users.noreply.github.com>
2022-02-24 20:44:13 +0100

    fix: jinja docstring in hook?

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,2 +1,4 @@
+"""
 {{ cookiecutter.update({"apis": cookiecutter.apis.split(',')}) }}
 {{ cookiecutter.update({"project_slug": cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}
+"""
